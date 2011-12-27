5e2c87c094a1979d26ba9acbe8bdfbaafe005272
Pawel Jankowski <pawel.jankowski@me.com>
2011-12-27 19:37:56 +0100

    Initial Commit

---
+++ DFN/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  DFN
+//
+//  Created by Paweł Jankowski on 2011-12-27.
+//  Copyright (c) 2011 pawel.jankowski@me.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ DFN/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DFN
+//
+//  Created by Paweł Jankowski on 2011-12-27.
+//  Copyright (c) 2011 pawel.jankowski@me.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
