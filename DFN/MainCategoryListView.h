d1073792a5dc08f9cb77cf93af7709625883253d
Radek <radekwilczak@gmail.com>
2012-03-17 18:56:56 +0100

    added MainCategoryListView class

---
+++ DFN/MainCategoryListView.h
@@ -0,0 +1,13 @@
+//
+//  MainCategoryListView.h
+//  DFN
+//
+//  Created by Radoslaw Wilczak on 17.03.2012.
+//  Copyright (c) 2012 pawel.jankowski@me.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainCategoryListView : UITableViewController
+
+@end
